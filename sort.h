e2a93dc0f6d837b14c1fdac151cc8728c417262d
sossesam <sossesam@gmail.com>
2023-09-30 02:22:22 -0700

    all

--- sort.h
+++ sort.h
@@ -25,5 +25,5 @@ void insertion_sort_list(listint_t **list);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+void shell_sort(int *array, size_t size);
 #endif
