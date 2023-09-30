da1dcf77b6064a07547eeb4cb999d5bb10c6def2
sossesam <sossesam@gmail.com>
2023-09-30 01:49:34 -0700

    al

--- sort.h
+++ sort.h
@@ -1,6 +1,5 @@
 #ifndef SORT_H
 #define SORT_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -12,19 +11,19 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
 void insertion_sort_list(listint_t **list);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
-#endif 
+#endif
